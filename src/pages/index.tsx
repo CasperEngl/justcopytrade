537eed85b76c726f8c537a82293c991261afdf7b
Casper Engelmann <me@casperengelmann.com>
2021-07-01 17:34:12 +0200

    making this thing actually usable.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from 'components/Logo'
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center py-32">
+    <section className="flex flex-col items-center justify-center py-64">
       <h1 className="sr-only">JustCopyTrade.com</h1>
       <Logo className="w-full max-w-3xl" />
       <p className="mt-8 text-3xl font-semibold text-gray-300">
